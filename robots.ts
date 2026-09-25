4df0b6708aebd31ef60f8aef9de8d5a5797ea677
luzzoni47-ai <luzzoni47@gmail.com>
2026-09-25 17:02:18 +0530

    Fix formatting in robots.ts

--- robots.ts
+++ robots.ts
@@ -1,11 +1,1 @@
-import { MetadataRoute } from 'next'
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://tankmetrics.in/sitemap.xml',
-  }
-}
+sitemap: 'https://tankmetrics.in/sitemap.xml',
